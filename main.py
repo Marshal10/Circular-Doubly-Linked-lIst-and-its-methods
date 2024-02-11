28e1e49da3ed6969c985116ac284d6963cb0ae15
Marshal10 <marshalmaleane123@gmail.com>
2024-02-11 17:27:28 +0530

    Delete a node

--- main.py
+++ main.py
@@ -85,14 +85,52 @@ class CircularDoublyLinkedList:
             index+=1
             current=current.next
         return -1
+    
+    def remove(self,index):
+        if self.head is None or index <0 or index >= self.length:
+            return None
+        if index==0:
+            if self.tail==self.head:
+                self.head.prev=None
+                self.head.next=None
+                self.head=None
+                self.tail=None
+            else:
+                self.head=self.head.next
+                self.head.prev=self.tail
+                self.tail.next=self.head
+        elif index==self.length-1:
+            if self.tail==self.head:
+                self.head.prev=None
+                self.head.next=None
+                self.head=None
+                self.tail=None
+            else:
+                self.tail=self.tail.prev
+                self.tail.next=self.head
+                self.head.prev=self.tail
+        else:
+            temp=self.head
+            for _ in range(index-1):
+                temp=temp.next
+            popped_node=temp.next
+            temp.next=popped_node.next
+            popped_node.next.prev=temp
+        self.length-=1
             
     
 cd_linked_list=CircularDoublyLinkedList()
 cd_linked_list.createDLL(10)
 print([node.value for node in cd_linked_list])
 cd_linked_list.insert(1,40)
+cd_linked_list.insert(1,90)
+cd_linked_list.insert(2,100)
 cd_linked_list.insert(0,70)
 print([node.value for node in cd_linked_list])
 # cd_linked_list.traversal()
 # cd_linked_list.reverseTraversal()
-print(cd_linked_list.search(100))
+# print(cd_linked_list.search(100))
+cd_linked_list.remove(2)
+print([node.value for node in cd_linked_list])
+cd_linked_list.remove(2)
+print([node.value for node in cd_linked_list])
